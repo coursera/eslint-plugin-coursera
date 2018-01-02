27dd85426b6eab33674703c8e84526e229098f18
Sumit Gogia <sgogia@coursera.org>
2018-01-02 14:02:25 -0800

    tabs to spaces

--- src/rules/no-lazy-build-loader.js
+++ src/rules/no-lazy-build-loader.js
@@ -9,35 +9,35 @@
 //------------------------------------------------------------------------------
 
 module.exports = {
-	meta: {
-		docs: {
-			description: "Disallow usage of LazyBuild in production.",
-			recommended: false
-		},
-	},
+  meta: {
+    docs: {
+      description: "Disallow usage of LazyBuild in production.",
+      recommended: false
+    },
+  },
 
-	create: function create(context) {
-		return {
-			'CallExpression': function CallExpression(node) {
-				var callee = node.callee;
-				var args = node.arguments;
+  create: function create(context) {
+    return {
+      'CallExpression': function CallExpression(node) {
+        var callee = node.callee;
+        var args = node.arguments;
 
-				if (callee.name === 'require' && args[0].value.startsWith("lazy-build-loader!")) {
-					context.report({
-						message: "The `lazy-build-loader` cannot be used in production. Please switch back to `lazy`.",
-						node: node
-					});
-				}
-			},
-			'ImportDeclaration': function ImportDeclaration(node) {
-				var source = node.source.value;
-				if (source.startsWith("lazy-build-loader!")) {
-					context.report({
-						message: "The `lazy-build-loader` cannot be used in production. Please switch back to `lazy`.",
-						node: node
-					});
-				}
-			}
-		};
-	}
+        if (callee.name === 'require' && args[0].value.startsWith("lazy-build-loader!")) {
+          context.report({
+            message: "The `lazy-build-loader` cannot be used in production. Please switch back to `lazy`.",
+            node: node
+          });
+        }
+      },
+      'ImportDeclaration': function ImportDeclaration(node) {
+        var source = node.source.value;
+        if (source.startsWith("lazy-build-loader!")) {
+          context.report({
+            message: "The `lazy-build-loader` cannot be used in production. Please switch back to `lazy`.",
+            node: node
+          });
+        }
+      }
+    };
+  }
 };
